2249243f136a1dd24349e5e482f2f73567ab227d
Alejandro <frozmiz@live.com>
2023-05-29 19:46:48 +0200

    feat: configurate hooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,23 +1,36 @@
 
 import { useEffect, useState } from 'react';
 import getGifs from './../helpers/GetGifs';
+import Title from './../../../cv/session1/src/components/Title';
 
 const GifGrid = ({ category }) => {
 
-  const [counter, setCounter] = useState(10);
+  const [images, setImages] = useState([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs( category );
+    setImages(newImages);
+  }
+
 
   useEffect( () => {
-    getGifs(category);
+    getImages();
   }, [])
  
 
   return (
     <>
       <h3> { category } </h3>
-      <h4> { counter } </h4>
-      <button onClick={ () => setCounter( counter + 1) }> +1 </button>
+
+      <ol>
+        { images.map( ({ id, title }) => (
+          <li key={ id }> {title} </li>
+        )) }
+        
+      </ol>
+      
     </>
   )
 }
 
-export default GifGrid
+export default GifGrid;
--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -3,13 +3,18 @@ const getGifs = async( category ) => {
     const resp = await fetch( url );
     const { data } = await resp.json();
 
+    
+
     const gifs = data.map( img => ({
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
     }))
-
+    
     return gifs;
+    
 }
 
-export default getGifs
+
+
+export default getGifs;
