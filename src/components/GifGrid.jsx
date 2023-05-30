8bb05eec839c26f09cc7a173768e6ff0db026d5e
Alejandro <frozmiz@live.com>
2023-05-30 21:40:07 +0200

    feat: loading images message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,10 +8,12 @@ import getGifs from './../helpers/GetGifs';
 const GifGrid = ({ category }) => {
 
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getImages = async() => {
     const newImages = await getGifs( category );
     setImages(newImages);
+    setIsLoading(false);
   }
 
 
@@ -23,6 +25,10 @@ const GifGrid = ({ category }) => {
   return (
     <>
       <h3> { category } </h3>
+
+      {
+        isLoading ? ( <h2> Cargando... </h2> ) : null 
+      }
 
       <div className='card-grid'>
         { images.map( ( image ) => (
--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 const GifItem = ({ title, url, id}) => {
 
-  console.log({title, url, id});
-
   return (
     <div className="card">
       <img src={ url } alt= { title } />
