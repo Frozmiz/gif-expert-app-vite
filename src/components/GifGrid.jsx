cbbf4b63a29a80e4d49ea5f6b469295cd4f02593
Alejandro <frozmiz@live.com>
2023-10-24 20:17:40 +0200

    feat: add loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -12,10 +12,10 @@ const GifGrid = ({ category }) => {
   return (
     <>
       <h3> { category } </h3>
-
       {
-        isLoading ? ( <h2> Loading... </h2> ) : null 
+        isLoading && ( <h2> Loading... </h2> )
       }
+      
 
       <div className='card-grid'>
         { images.map( ( image ) => (
